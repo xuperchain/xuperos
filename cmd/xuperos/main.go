1bd4acbfab66cf9e0442a335ec13ccd6e4522407
chunhui01 <chunhui01@outlook.com>
2021-01-28 15:34:38 +0800

    add Makefile and auto tools

--- cmd/xuperos/main.go
+++ cmd/xuperos/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/xuperchain/xuperos/cmd/xuperos/cmd"
 
 	"github.com/spf13/cobra"
+)
+
+var (
+	Version   = ""
+	BuildTime = ""
+	CommitID  = ""
 )
 
 func main() {
@@ -21,18 +28,42 @@ func main() {
 
 func NewServiceCommand() (*cobra.Command, error) {
 	rootCmd := &cobra.Command{
-		Use:           "xuperos <command> [arguments]",
-		Short:         "xuperos is a blockchain network building service.",
-		Long:          "xuperos is a blockchain network building service.",
+		Use:           "xchain <command> [arguments]",
+		Short:         "xchain is a blockchain network building service.",
+		Long:          "xchain is a blockchain network building service.",
 		SilenceUsage:  true,
 		SilenceErrors: true,
-		Example:       "xuperos startup --conf /home/rd/xuperos/conf/env.yaml",
+		Example:       "xchain startup --conf /home/rd/xchain/conf/env.yaml",
 	}
 
 	// cmd service
 	rootCmd.AddCommand(cmd.GetStartupCmd().GetCmd())
 	// cmd version
-	rootCmd.AddCommand(cmd.GetVersionCmd().GetCmd())
+	rootCmd.AddCommand(GetVersionCmd().GetCmd())
 
 	return rootCmd, nil
+}
+
+type versionCmd struct {
+	cmd.BaseCmd
+}
+
+func GetVersionCmd() *versionCmd {
+	versionCmdIns := new(versionCmd)
+
+	subCmd := &cobra.Command{
+		Use:     "version",
+		Short:   "view process version information.",
+		Example: "xchain version",
+		Run: func(cmd *cobra.Command, args []string) {
+			versionCmdIns.PrintVersion()
+		},
+	}
+	versionCmdIns.SetCmd(subCmd)
+
+	return versionCmdIns
+}
+
+func (t *versionCmd) PrintVersion() {
+	fmt.Printf("%s-%s %s\n", Version, CommitID, BuildTime)
 }
